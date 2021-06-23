57c15ee0aa7f25fb223f7bbe9e5a3e1742ab713f
DELL <sandeshlad67@gmail.com>
2021-06-23 16:11:35 +0530

    BroadcastReceiver Using Kotlin.

---
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var wifiSwitch: Switch
+    lateinit var wifiManager: WifiManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        title = "BroadcastReceiver"
+        wifiSwitch = findViewById(R.id.wifiSwitch)
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifiSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                wifiManager.isWifiEnabled = true
+                wifiSwitch.text = "WiFi is ON"
+            } else {
+                wifiManager.isWifiEnabled = false
+                wifiSwitch.text = "WiFi is OFF"
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        val intentFilter = IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        registerReceiver(wifiStateReceiver, intentFilter)
+    }
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(wifiStateReceiver)
+    }
+    private val wifiStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                WifiManager.WIFI_STATE_UNKNOWN)) {
+                WifiManager.WIFI_STATE_ENABLED -> {
+                    wifiSwitch.isChecked = true
+                    wifiSwitch.text = "WiFi is ON"
+                    Toast.makeText(this@MainActivity, "Wifi is On", Toast.LENGTH_SHORT).show()
+                }
+                WifiManager.WIFI_STATE_DISABLED -> {
+                    wifiSwitch.isChecked = false
+                    wifiSwitch.text = "WiFi is OFF"
+                    Toast.makeText(this@MainActivity, "Wifi is Off", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+}
